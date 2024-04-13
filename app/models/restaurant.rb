304984ceddcea5f8ea1825873e21ad08bf7dde3d
Loic Jeannin <inklo.ca@gmail.com>
2024-04-13 13:07:18 -0400

    yelp map

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name :adress :categorie, presence: true
+  validates :categorie, inclusion {in:["chinese", "italian", "japanese", "french", "belgian"].}
 end
