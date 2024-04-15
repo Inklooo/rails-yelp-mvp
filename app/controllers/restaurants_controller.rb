3139f72a3d6ccfdd905f59b4a1fabde5f748f973
Loic Jeannin <inklo.ca@gmail.com>
2024-04-15 14:36:27 -0400

    yelp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,11 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -11,6 +13,16 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
   end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
 
 end
